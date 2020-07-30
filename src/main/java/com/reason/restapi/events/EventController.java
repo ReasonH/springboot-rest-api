1f4f926b858dcdb97278cc544686e7de65267af2
YooHyuk Lee <azsxd34@naver.com>
2020-07-31 02:21:40 +0900

    get event list API implements

--- src/main/java/com/reason/restapi/events/EventController.java
+++ src/main/java/com/reason/restapi/events/EventController.java
@@ -2,6 +2,9 @@ package com.reason.restapi.events;
 
 import com.reason.restapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -9,6 +12,7 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +60,17 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(Link.of("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler){
+        Page<Event> page = this.eventRepository.findAll(pageable);
+        // entity를 event resource로 변환하여 받아옴으로써 이벤트마다 self description 달성
+        var pageModels = assembler.toModel(page, entity -> EventResource.getEventResource(entity));
+
+        // Model로 변경 시 링크 추가 가능한 메서드가 생긴다.
+        pageModels.add(Link.of("/docs/index.html#resources-events-list").withRel("profile"));
+        return ResponseEntity.ok(pageModels);
     }
 
     private ResponseEntity<?> badRequest(Errors errors) {
